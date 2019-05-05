ee27dc7967fb2094e47ea3de747abc8130c79e56
ralph.hsu <fc.nothung@gmail.com>
2019-05-05 16:47:29 +0800

    Add sort of test cases

--- 01/LibTest.php
+++ 01/LibTest.php
@@ -18,4 +18,29 @@ class LibTest extends TestCase
     {
         $this->assertEquals(2, adder(1, 1));
     }
+
+    public function testAddNegativeInteger()
+    {
+        $this->assertEquals(-1, adder(0, -1));
+    }
+
+    public function testAddFloat()
+    {
+        $this->expectException(Exception::class);
+
+        adder(0.1, 1);
+        adder(0.1, 0.1);
+        adder(1, 0.1);
+    }
+
+    public function testAddWrongType()
+    {
+        $this->expectException(Exception::class);
+
+        adder(null, 1);
+        adder(1, null);
+        adder(null, null);
+        adder('1', 1);
+        adder(1, '1');
+    }
 }
--- 01/lib.php
+++ 01/lib.php
@@ -2,7 +2,7 @@
 
 function adder($a, $b) {
     if (!is_int($a) || !is_int($b)) {
-        throw new Excetiob('wrong type');
+        throw new Exception('wrong type');
     }
 
     return $a + $b;
