3c67c052b50961e09193e8b1fd737cffec143ed2
Zero <john.royer@gmail.com>
2019-05-06 10:03:30 +0800

    Add target to test

---
+++ 02/checker.php
@@ -0,0 +1,22 @@
+<?php
+
+function scoreChecker(int $score)
+{
+    if (101 > $score || -1 < $score) {
+        throw new Exception();
+    }
+
+    if (90 > $score) {
+        return 'A';
+
+    } elseif (80 > $score) {
+        return 'B';
+
+    } elseif (70 > $score) {
+        return 'C';
+
+    } elseif (60 > $score) {
+        return 'D';
+    }
+    return 'F';
+}
